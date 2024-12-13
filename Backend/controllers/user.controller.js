48a111e714618b1b7ef80adcbc6308f6cb9e6f55
Dennis <kwesimodestygh111@gmail.com>
2024-12-12 21:52:09 -0800

    User login done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -24,3 +24,32 @@ module.exports.registerUser = async(req, res, next) => {
     res.status(200).json({token, user})
 
 } 
+
+module.exports.loginUser = async (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        // Find user and explicitly select the password field
+        const user = await userModel.findOne({ email }).select('+password');
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        // Compare password
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        // Generate token and return success response
+        const token = user.generateAuthToken();
+        res.status(200).json({ token, user });
+    } catch (err) {
+        next(err); // Pass error to the next middleware (error handler)
+    }
+};
